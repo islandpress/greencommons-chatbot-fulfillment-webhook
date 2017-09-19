f43574da9ca0ed149083b416298d36dd038f1524
George Tourtellot <tourtelt@gmail.com>
2017-09-19 16:49:09 -0400

    Add basic support for GC querying, along with tests.

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ from urllib.error import HTTPError
 
 import json
 import os
+import requests
+import random
 
 from flask import Flask
 from flask import request
@@ -40,13 +42,154 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    func = request_dispatch(req)
+    res = {}
+    if func:
+        res = func(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
+
+
+# def webhook():
+#     req = request.get_json(silent=True, force=True)
+
+#     print("Request:")
+#     print(json.dumps(req, indent=4))
+
+#     res = processRequest(req)
+
+#     res = json.dumps(res, indent=4)
+#     # print(res)
+#     r = make_response(res)
+#     r.headers['Content-Type'] = 'application/json'
+#     return r
+
+
+def request_dispatch(req):
+    if req.get("result").get("action") == "how_many_resources_response":
+        return handle_howmanyresources
+    if req.get("result").get("action") == "show_me_response":
+        return handle_showme
+    if req.get("result").get("action") == "what_is_response":
+        return handle_whatis
+    return None
+
+
+def get_all_resource_types():
+    return ['articles','books','reports','urls','audios',
+            'courses','datasets','images','syllabuses',
+            'videos','profiles']
+
+
+def get_resource_type_singular_dict():
+    return {'articles': 'article',
+            'books': 'book',
+            'reports': 'report',
+            'urls': 'url',
+            'audios': 'audio file',
+            'courses': 'course',
+            'datasets': 'dataset',
+            'images': 'image',
+            'syllabuses': 'syllabus',
+            'videos': 'video',
+            'profiles': 'profile'}
+
+
+def handle_howmanyresources(req):
+    q = req.get("result").get("parameters").get("eco-topics")
+    if not q:
+        return {}
+
+    #resource_type = [req.get("result").get("parameters").get("resource_types")]
+    all_resource_types = get_all_resource_types()
+
+    resource_type = ','.join(all_resource_types)
+    model_types = ['resources'] #, 'networks', 'lists']
+    target_page = 1
+    per_page = 1
+    url = 'https://greencommons.herokuapp.com/api/v1/search?q={}' \
+          '&filters[resource_types]={}' \
+          '&filters[model_types]={}&page={}&per={}'.format(
+            q, resource_type, ','.join(model_types), target_page, per_page)
+    r = requests.get(url)
+    page = per = 0
+    if r.ok:
+        j = r.json()
+        last = j.get("links", {}).get("last")
+        page = int(last.split("page=")[-1].split("&")[0])
+        per = int(last.split("per=")[-1])
+    approx_total = page*per
+
+    speech = "Green Commons has {} resources on {}.".format(
+        approx_total, q)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "greencommons-chatbot-fulfillment-webhook"
+    }
+
+
+def handle_showme(req):
+    q = req.get("result").get("parameters").get("eco-topics")
+    if not q:
+        return {}
+
+    resource_type = req.get("result").get("parameters").get("resource_types")
+    if not resource_type or resource_type == "":
+        resource_type = ','.join(get_all_resource_types())
+    model_types = ['resources'] #, 'networks', 'lists']
+    target_page = 1
+    per_page = 10
+    url = 'https://greencommons.herokuapp.com/api/v1/search?q={}' \
+          '&filters[resource_types]={}' \
+          '&filters[model_types]={}&page={}&per={}'.format(
+            q, resource_type, ','.join(model_types),
+            target_page, per_page)
+    r = requests.get(url)
+    data = None
+    if r.ok:
+        data = r.json().get("data")
+    if not data:
+        speech = "I couldn't find {} resources about {}.".format(resource_type, q)
+    else:        
+        d = random.choice(data).get("attributes")
+        content = d.get("title", "")
+        if d.get("short_content"):
+            content += "\n" + d.get("short_content")
+        if d.get("resource_url"):
+            content += "\n" + d.get("resource_url")
+        singular_resource_type = get_resource_type_singular_dict().get(
+            resource_type, "resource")
+        speech = "Checkout this {} about {}:\n{}".format(singular_resource_type,
+                                                         q, content)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "greencommons-chatbot-fulfillment-webhook"
+    }
+
+
+def handle_whatis(req):
+    q = req.get("result").get("parameters").get("eco-topics")
+    if not q:
+        return {}
+    speech = "Right now I can't tell you anything about {}. "\
+             "Ask me again once I've learned more.".format(q)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "greencommons-chatbot-fulfillment-webhook"
+    }
 
 
 def processRequest(req):
@@ -98,8 +241,9 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Right now in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Right now in " + location.get('city') + ": " + \
+             condition.get('text') + ", the temperature is " + \
+             condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -109,7 +253,7 @@ def makeWebhookResult(data):
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "greencommons-chatbot-fulfillment-webhook"
     }
 
 
