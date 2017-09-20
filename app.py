65c3edf14e89b41df27ba0bc6306a454c931c0a8
George Tourtellot <tourtelt@gmail.com>
2017-09-19 22:56:48 -0400

    Debugging handle_howmanyresources

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
+# from urllib.parse import urlparse, urlencode
+# from urllib.request import urlopen, Request
+# from urllib.error import HTTPError
 
+import requests
 import json
 import os
-#import requests
 
 from flask import Flask
 from flask import request
@@ -189,68 +189,68 @@ def handle_whatis(req):
     }
 
 
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+# def processRequest(req):
+#     if req.get("result").get("action") != "yahooWeatherForecast":
+#         return {}
+#     baseurl = "https://query.yahooapis.com/v1/public/yql?"
+#     yql_query = makeYqlQuery(req)
+#     if yql_query is None:
+#         return {}
+#     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+#     result = urlopen(yql_url).read()
+#     data = json.loads(result)
+#     res = makeWebhookResult(data)
+#     return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+# def makeYqlQuery(req):
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     city = parameters.get("geo-city")
+#     if city is None:
+#         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+#     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
+# def makeWebhookResult(data):
+#     query = data.get('query')
+#     if query is None:
+#         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
+#     result = query.get('results')
+#     if result is None:
+#         return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
+#     channel = result.get('channel')
+#     if channel is None:
+#         return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
+#     item = channel.get('item')
+#     location = channel.get('location')
+#     units = channel.get('units')
+#     if (location is None) or (item is None) or (units is None):
+#         return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
+#     condition = item.get('condition')
+#     if condition is None:
+#         return {}
 
-    # print(json.dumps(item, indent=4))
+#     # print(json.dumps(item, indent=4))
 
-    speech = "Right now in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+#     speech = "Right now in " + location.get('city') + ": " + condition.get('text') + \
+#              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    print("Response:")
-    print(speech)
+#     print("Response:")
+#     print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
+#     return {
+#         "speech": speech,
+#         "displayText": speech,
+#         # "data": data,
+#         # "contextOut": [],
+#         "source": "apiai-weather-webhook-sample"
+#     }
 
 
 if __name__ == '__main__':
